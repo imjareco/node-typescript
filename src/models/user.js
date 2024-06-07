b44790ddbc37a23ddf69640dee1467abc21a4f6d
fercut <ferminycia@gmail.com>
2024-06-07 10:37:28 +0200

    update user-model

--- src/models/user.js
+++ src/models/user.js
@@ -2,17 +2,17 @@
 const { Schema, model } = require('mongoose');
 
 const UserSchema = Schema({
-  name: {type: String, required: true,},
-  lastname: {type: String, required: [true, `The lastname is required`],},
-  username: {type: String,required: false ,unique: true,},
-  email: {type: String,required: true,unique: true,},
-  phone: {type: Number, require:true, unique:true, trim: true},
-  adress: {type: String, require: true},
-  location: {type: String, require: true, trim: true},
-  city: {type: String, require: true, trim: true},
-  postalCode: {type: Number, require: true, trim: true},
-  password: {type: String,required: true,},
-  
+  name: { type: String, required: true, },
+  lastname: { type: String, required: [true, `The lastname is required`], },
+  username: { type: String, required: false, unique: true, },
+  email: { type: String, required: true, unique: true, },
+  phone: { type: Number, require: true, unique: true, trim: true },
+  adress: { type: String, require: true },
+  location: { type: String, require: true, trim: true },
+  city: { type: String, require: true, trim: true },
+  postalCode: { type: Number, require: true, trim: true },
+  password: { type: String, required: true, },
+
   status: {
     type: String,
     default: 'ACTIVE',
@@ -25,9 +25,20 @@ const UserSchema = Schema({
     enum: ['ADMIN_ROLE', 'USER_ROLE'],
   },
 
+  cart: [
+    {
+      itemId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Article',
+        default: undefined,
+      },
+      quantity: { Number, default: 1},
+    },
+  ],
+
   lastSession: { type: Date },
   birthday: { type: Date },
-}, { timestamps: true});
+}, { timestamps: true });
 
 UserSchema.methods.toJSON = function () {
   const { _id, __v, password, ...user } = this.toObject();
